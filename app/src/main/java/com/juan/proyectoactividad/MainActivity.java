eab98796d348d1d9b5dda20b896f61d8b4aff7eb
Juan <juand200528@gmail.com>
2025-03-25 09:37:05 -0500

    Inicializando rama develop

---
+++ app/src/main/java/com/juan/proyectoactividad/MainActivity.java
@@ -0,0 +1,20 @@
+package com.juan.proyectoactividad;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.listView);
+        String[] nombres = {"Juan Rengifo", "Angel Duban", "Paula Chapeño", "Juan Meneses", "Eduar Jimenez", "Kevin Penagos", "Melissa Muñoz"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, nombres);
+        listView.setAdapter(adapter);
+    }
+}
